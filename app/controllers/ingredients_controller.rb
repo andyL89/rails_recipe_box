45e21744cbde9b74d64a36dfa0e41f8a69f8b14e
Andy Lawson <alawson89@gmail.com>
2021-03-17 11:41:12 -0700

    add controllers, routes, and views
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,53 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+    render :index
+  end
+
+  def new
+    @ingredient = Ingredient.new
+    render :new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:notice] = "Ingredient added!"
+      redirect_to ingredients_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+    render :show
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(ingredient_params)
+      flash[:notice] = "Ingredient updated!"
+      redirect_to ingredients_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+    flash[:notice] = "Ingredient deleted!"
+    redirect_to ingredients_path
+  end
+
+  private
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'recipes#index'
+  resources :recipes do
+    resources :ingredients, :tags
+  end
+  resources :tags do
+    resources :recipes
+  end
+  resources :ingredients do
+    resources :recipes
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
